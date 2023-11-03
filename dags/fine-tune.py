eeb1048e9d311e45bfcd1a13103ff744417fa2f1
KiraKi <kirakotkova@gmail.com>
2023-11-03 13:28:35 +0300

    d

--- dags/fine-tune.py
+++ dags/fine-tune.py
@@ -51,7 +51,20 @@ with DAG(
 
         print(generator("Вопрос: Что такое учебный центр Neoflex? Ответ: ", do_sample=True, max_length=200))
 
-        
+    
+    finetune_this = BashOperator(
+        task_id="finetune",
+        bash_command="""python run_clm.py 
+        --model_name_or_path sberbank-ai/rugpt3small_based_on_gpt2 
+        --train_file train.txt 
+        --per_device_train_batch_size 1 
+        --block_size 2048 
+        --dataset_config_name plain_text 
+        --do_train 
+        --num_train_epochs 20 
+        --output_dir models/rugpt3small 
+        --overwrite_output_dir""",
+)
 
     
     finetune_model_task = PythonOperator(
@@ -64,4 +77,4 @@ with DAG(
     )
 
 
-finetune_model_task
+finetune_this >> finetune_model_task
